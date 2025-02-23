2dd95d062ca23a368e2ee88c22929f7004d4a1a0
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-02-23 17:01:18 -0500

    añadir movimiento horizontal rectilineo uniforme a los enemigos

--- UnityProject/SpaceInvadersClone/Assets/Scripts/Enemies/EnemyController.cs
+++ UnityProject/SpaceInvadersClone/Assets/Scripts/Enemies/EnemyController.cs
@@ -6,6 +6,10 @@ public class EnemyController : MonoBehaviour
 
     private GameManager gameManager;
 
+
+    private float direction;
+
+    public float speed = 1.0f;
     void Awake()
     {
         if (Instance == null)
@@ -22,11 +26,29 @@ public class EnemyController : MonoBehaviour
     void Start()
     {
         gameManager = GameManager.Instance;
+
+        direction = 1f;
+    }
+
+    void Update()
+    {
+        EnemiesMovement();
     }
 
 
     private void OnDestroy()
     {
         Instance = null;
+    }
+
+    public void EnemiesMovement()
+    {
+        /**
+         * Mueve a los enemigos hacia la izquierda
+         * y hacia abajo, en caso de que lleguen a los
+         * límites de la pantalla.
+         */
+
+        transform.position += Vector3.right * direction * speed * Time.deltaTime;
     }
 }
