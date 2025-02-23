0169b08ba19b5e8ef80242036c5ab6caca8b97c3
AndresFelipeMunozAguilar <munozaguilarandresfelipe@gmail.com>
2025-02-23 12:41:42 -0500

    añadir funcionalidades al movimiento de la bala y al disparo. Estas son las siguientes:
    destruir a la bala cuando salga de la pantalla y se le ponga un offset
    
    Añadir un tiempo de recarga a cada disparo, asi, sólo se puede disparar cada cierto tiempo

---
+++ UnityProject/SpaceInvadersClone/Assets/Scripts/Bullet/Bullet.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 0.1f;
+
+    private Vector2 direction;
+
+    private Vector2 screenBounds;
+    private float upperBound;
+
+    // Distancia a la que la bala se destruirá
+    // con respecto al límite superior de la pantalla
+    public float despawningOffset;
+
+    private float bulletHalfHeight;
+
+    private float outsideCondition;
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        upperBound = screenBounds.y;
+        bulletHalfHeight = GetComponent<SpriteRenderer>().bounds.size.y / 2;
+        outsideCondition = upperBound + bulletHalfHeight + despawningOffset;
+
+        direction = transform.up;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    public void Movement()
+    {
+
+        // Mueve el objeto hacia arriba
+
+        // Mueve la bala, de manera proporcional al tiempo
+        // entre frames
+        transform.position += (Vector3)direction * speed * Time.deltaTime;
+
+        if (transform.position.y > outsideCondition)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- UnityProject/SpaceInvadersClone/Assets/Scripts/Player/Shooting.cs
+++ UnityProject/SpaceInvadersClone/Assets/Scripts/Player/Shooting.cs
@@ -4,12 +4,19 @@ public class Shooting : MonoBehaviour
 {
     public Transform firePoint;
 
+
     public GameObject bulletPrefab;
 
     // Variable para almacenar la tecla de disparo
     public KeyCode shootKey = KeyCode.Space;
 
     private bool isShooting = false;
+
+
+    // Variables para controlar el tiempo de recarga
+    // en segundos
+    public float maxReloadTime = 5.0f;
+    private float currentReloadTime = 0.0f;
 
     void Start()
     {
@@ -22,10 +29,18 @@ public class Shooting : MonoBehaviour
         // Verifica si la tecla de disparo está siendo presionada
         isShooting = Input.GetKey(shootKey);
 
-        if (isShooting)
+        if (isShooting && currentReloadTime <= 0)
         {
             Shoot();
+            currentReloadTime = maxReloadTime;
         }
+
+        if (currentReloadTime > 0)
+        {
+            currentReloadTime -= Time.deltaTime;
+        }
+
+        Debug.Log("Current Reload Time: " + currentReloadTime);
     }
 
     public void Shoot()
